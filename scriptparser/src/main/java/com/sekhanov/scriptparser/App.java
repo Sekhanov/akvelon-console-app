3f27c6789b941b2b61614f8ceb052a67d24b8306
Sergey Khanov (home) <sekhanov@gmail.com>
2019-10-22 21:22:29 +0300

    parse commands

--- scriptparser/src/main/java/com/sekhanov/scriptparser/App.java
+++ scriptparser/src/main/java/com/sekhanov/scriptparser/App.java
@@ -1,20 +1,21 @@
 package com.sekhanov.scriptparser;
 
-import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * Hello world!
  */
 public final class App {
     public static void main(String[] args) {
-        try (BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
-            ScriptParser scriptParser = new ScriptParser(br);
-            scriptParser.testFile();
+        try (Scanner fileScanner = new Scanner(new FileReader(args[0]))) {
+            ScriptParser scriptParser = new ScriptParser(fileScanner);
+            scriptParser.parseCommands();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
 
     }
 }
--- scriptparser/src/main/java/com/sekhanov/scriptparser/ScriptParser.java
+++ scriptparser/src/main/java/com/sekhanov/scriptparser/ScriptParser.java
@@ -1,27 +1,46 @@
 package com.sekhanov.scriptparser;
 
-import java.io.BufferedReader;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * ScriptParser
  */
 public class ScriptParser {
 
-    private BufferedReader br;
+    private List<String> commandList;
+    private Scanner fileScanner;
+    private Scanner inputScanner;
 
-    public ScriptParser(BufferedReader br) {
-        this.br = br;
+
+    public ScriptParser(Scanner fileScanner) {
+        this.fileScanner = fileScanner;
+        inputScanner = new Scanner(System.in);
+        commandList = new ArrayList<>();
     }
 
     public void testFile() {
-        try {
-            System.out.println(br.readLine());
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            while(fileScanner.hasNextLine()) {
+                System.out.println(fileScanner.nextLine());
+            }
+
+    }
+
+    public void parseCommands() {
+        String nextCommand = "";
+        while(fileScanner.hasNext()) {
+            fileScanner.useDelimiter("[\\n:]");
+            nextCommand = fileScanner.next().trim().toLowerCase();
+            if(nextCommand.equals("")) {
+                continue;
+            }
+            commandList.add(nextCommand);
         }
+        commandList.forEach(System.out::println);
     }
 
     
+
+
 }
