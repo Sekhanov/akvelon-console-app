0f48ed26c8437eb29d5303c2da157c46b756f17c
Sergey Khanov (home) <sekhanov@gmail.com>
2019-10-23 21:29:52 +0300

    maven

--- scriptparser/src/main/java/com/sekhanov/scriptparser/App.java
+++ scriptparser/src/main/java/com/sekhanov/scriptparser/App.java
@@ -9,15 +9,17 @@ import java.util.Scanner;
  */
 public final class App {
     public static void main(String[] args) {
-        try (Scanner fileScanner = new Scanner(new FileReader(args[0]))) {
-            ScriptExecutor scriptExecutor = new ScriptExecutor(fileScanner);
-            // scriptExecutor.testFile();
-            scriptExecutor.addStatements();
-            scriptExecutor.executeScript();
-        } catch (IOException e) {
-            e.printStackTrace();
+        if(args.length == 0) {
+            System.out.println("enter one argument with script file location or file name if the file is in the same directory with jar file");
+        } else {
+            try (Scanner fileScanner = new Scanner(new FileReader(args[0]))) {
+                ScriptExecutor scriptExecutor = new ScriptExecutor(fileScanner);
+                // scriptExecutor.testFile();
+                scriptExecutor.addStatements();
+                scriptExecutor.executeScript();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-
-
     }
 }
